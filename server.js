94c6561f7d0a78d2ac4a2e6d8ec3d282b060ba81
deun_0124 <m3g3ab@gmail.com>
2021-04-20 16:15:38 +0900

    database connection

--- server.js
+++ server.js
@@ -4,35 +4,33 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const fs = require('fs');
+const data = fs.readFileSync('./database.json')
+const conf =JSON.parse(data);
+
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host : conf.host,
+  user : conf.user,
+  password :conf.password,
+  database : conf.database
+
+})
+
+connection.connect();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/api/customers',(req, res)=>{
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '홍길동',
-            'birthday': '1999.01.01',
-            'gender': '남자',
-            'job': '대학생'
-          }, {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '홍길순',
-            'birthday': '1980.01.02',
-            'gender': '여자',
-            'job': '학생'
-          }, {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/3',
-            'name': '홍길돌',
-            'birthday': '1992.01.03',
-            'gender': '남자',
-            'job': '프로그래머'
-          }
-
-    ]);
+   connection.query(
+     "select * from customer",
+     (err, rows, fields) => {
+       res.send(rows);
+     }
+   )
 })
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`))
