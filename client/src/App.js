5e127374c759104a79edd18ed7dcfb2d0d887377
deun_0124 <m3g3ab@gmail.com>
2021-04-23 15:38:35 +0900

    FIlter Search

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,22 @@ import Axios from 'axios'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import CustomerAdd from './components/CustomerAdd'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
 
-import SearchAppBar from './components/SearchAppBar.js'
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
+import { LocalConvenienceStoreOutlined, TextRotationAngledownOutlined } from '@material-ui/icons';
 
 
 const styles = makeStyles((theme) => ({
   root: {
+    flexGrow: 1,
     width: '100%',
     marginTop: theme.spacing(3),
     minWidth: 1080
@@ -44,10 +54,56 @@ const styles = makeStyles((theme) => ({
     fontSize: '1.0rem'
   },
 
-
-
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
 }));
-
 
 
 
@@ -100,17 +156,99 @@ function App() {
   }
 
 
-  const lists = ['번호', '이미지', '이름', '생년월일', '성별', '직업', '설정']
 
+  // filteredComponents.map((c)=>{
+  //   return <Customer
+  //           stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name}birthday={c.birthday}gender={c.gender} job={c.job} />
+  // });
+  
+  //   return data.map((c)=>{
+  //     return <Customer
+  //        stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name}birthday={c.birthday}gender={c.gender} job={c.job} />
+  //   })
+  
+  // }
+  
+
+
+  const lists = ['번호', '이미지', '이름', '생년월일', '성별', '직업', '설정']
+  const classes = styles();
+
+  
+
+
+  // const SearchAppBar=()=> {
+    
+    const [state, setstate] = useState("")
+  
+  
+  
+    const handleValueChange = (e) =>{
+      
+  
+      setstate(e.currentTarget.value)
+      
+      
+  
+    }
+
+    console.log(state)
+  
+  // }
+
+  const filteredComponents = (data) =>{
+    data=data.filter((c)=>{
+      return c.name.indexOf(state)>-1;
+    })
+    return data.map((c)=>{
+      return  <Customer
+            stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name}birthday={c.birthday}gender={c.gender} job={c.job} />
+       
+    })
+  }
+  
 
   return (
     <div className={styles.root} >
 
-      <SearchAppBar />
+      {/* <SearchAppBar customers={customers}/> */}
+
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            고객관리시스템
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="고객 검색"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              name="state"
+              value={state}
+              onChange={handleValueChange}
+              
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          
+          </div>
+        </Toolbar>
+      </AppBar>
 
 
-
-      <div className={styles.menu} >
+      <div className={styles.menu} style={{justifyContent:'center', display:'flex', marginTop:'15px', marginBottom:'15px'}} >
         <CustomerAdd stateRefresh={stateRefresh} />
       </div>
    
@@ -137,22 +275,21 @@ function App() {
 
 
             {customers != 0 ?
-              customers.map(c => {
-                return (
+                filteredComponents(customers) 
 
-
-                  <Customer
-                    stateRefresh={stateRefresh}
-                    key={c.id}
-                    id={c.id}
-                    image={c.image}
-                    name={c.name}
-                    birthday={c.birthday}
-                    gender={c.gender}
-                    job={c.job}
-                  />
-                )
-              }) :
+                // filteredComponents.map((c) =>{
+                //   return <Customer
+                //             stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name}birthday={c.birthday}gender={c.gender} job={c.job} />
+                //   })
+                // customers.map(c => {
+                
+                //    return(
+ 
+                //     <Customer
+                //       stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name}birthday={c.birthday}gender={c.gender} job={c.job} />
+                 
+                //       )})
+               :
               <TableRow>
                 <TableCell colSpan="6" align="center">
                   <CircularProgress className={styles.progress} variant="determinate" value={completed} />
